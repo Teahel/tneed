1fca970c3986c22c704da8d2278827f8a8718165
Matthew.Li <teahel@163.com>
2020-11-20 17:59:32 +0800

    用户功能开发

---
+++ src/main/java/com/teahel/tneed/account/entity/User.java
@@ -0,0 +1,26 @@
+package com.teahel.tneed.account.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+
+/**
+ * @version 1.0
+ * @author： L.T.J
+ * @date： 2020-11-20
+ */
+
+@Data
+@Entity
+public class User {
+    private String userName;
+
+    private String passWord;
+
+    public User(String userName,String passWord){
+        this.passWord = passWord;
+        this.userName = userName;
+        throw new RuntimeException("");
+    }
+
+}
