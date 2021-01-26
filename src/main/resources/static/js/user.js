39d4a9ce63a93809648dc91ba7293ce2c064a51e
teahel <li.teahel@gmail.com>
2021-01-26 17:36:51 +0800

    修改密码界面显示

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -1,12 +1,44 @@
 new Vue({
     el: '#user',
     data: function() {
+        var validatePass = (rule, value, callback) => {
+            if (value === '') {
+                callback(new Error('请输入密码'));
+            } else {
+                if (this.ruleForm.checkPass !== '') {
+                    this.$refs.ruleForm.validateField('checkPass');
+                }
+                callback();
+            }
+        };
+        var validatePass2 = (rule, value, callback) => {
+            if (value === '') {
+                callback(new Error('请再次输入密码'));
+            } else if (value !== this.ruleForm.pass) {
+                callback(new Error('两次输入密码不一致!'));
+            } else {
+                callback();
+            }
+        };
         return {
             tableData: [{
                 username: '',
                 email: '',
                 effectiveTime: ''
-            }]
+            }],
+            dialogUserVisible: false,
+            ruleForm: {
+                pass: '',
+                checkPass: '',
+            },
+            rules: {
+                pass: [
+                    { validator: validatePass, trigger: 'blur' }
+                ],
+                checkPass: [
+                    { validator: validatePass2, trigger: 'blur' }
+                ],
+            }
         }
     },
     created:function(){
@@ -37,6 +69,30 @@ new Vue({
            }).catch(function (error) {
                console.log(error);
            });
-       }
+       },
+       updateUser:function () {
+
+       },
+        handleClose(done) {
+            this.$confirm('确认关闭？')
+                .then(_ => {
+                done();
+        })
+        .catch(_ => {});
+        },
+        submitForm:function(formName) {
+            this.$refs[formName].validate((valid) => {
+                if (valid) {
+                    alert('submit!');
+                } else {
+                    console.log('error submit!!');
+            return false;
+        }
+        });
+        },
+        resetForm:function(formName) {
+            this.$refs[formName].resetFields();
+        }
+       
     }
 })
